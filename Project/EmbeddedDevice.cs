c523127730649265052007ffe9b6670fb9eb0097
Juliusz Trochimczuk <assasindal@gmail.com>
2025-03-16 20:47:40 +0100

    Writing embedded device

--- Project/EmbeddedDevice.cs
+++ Project/EmbeddedDevice.cs
@@ -2,14 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Project
 {
     public class EmbeddedDevice : Device
     {
-        public EmbeddedDevice(string id, string name, bool isTurnedOn) : base(id, name, isTurnedOn)
+        private string IpAdress { get; set; }
+        private string NetworkName { get; set; }
+
+        public EmbeddedDevice(string id, string name, string ipAdress, string networkName) : base(id, name, false)
         {
+            //Regex expression for IPv4
+            Regex regex = new("\"(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\"");
+            if (!regex.IsMatch(ipAdress))
+                throw new ArgumentException();
+
+            IpAdress = ipAdress;
+            NetworkName = networkName;
         }
+
+        public override void TurnOn()
+        {
+            Connect();
+            base.TurnOn();
+        }
+
+        private void Connect()
+        {
+            if (NetworkName.Contains("MD Ltd."))
+                throw new ConnectionException();
+        }
+    }
+
+    public class ConnectionException : Exception 
+    {
+        public ConnectionException() : base("Network name is wrong") { }
     }
 }
