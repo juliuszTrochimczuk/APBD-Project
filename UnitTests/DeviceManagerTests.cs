e8a22d576a868a35bbffb6cbed22615b78a2904a
Juliusz Trochimczuk <assasindal@gmail.com>
2025-04-09 20:20:28 +0200

    Cleaning project structure

--- UnitTests/DeviceManagerTests.cs
+++ UnitTests/DeviceManagerTests.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Controllers;
 
-namespace Tests
+namespace UnitTests
 {
     public class DeviceManagerTests
     {
--- UnitTests/DeviceTests.cs
+++ UnitTests/DeviceTests.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 using Devices;
 
-namespace Tests
+namespace UnitTests
 {
     public class DeviceTests
     {
